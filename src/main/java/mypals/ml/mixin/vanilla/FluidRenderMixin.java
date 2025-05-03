ee656f2239f5283af1fcb400541434faf7f3f449
hotpad100c <46704388+hotpad100c@users.noreply.github.com>
2025-05-03 12:28:04 +0900

    1.21.1 -fluid transparency

--- src/main/java/mypals/ml/mixin/vanilla/FluidRenderMixin.java
+++ src/main/java/mypals/ml/mixin/vanilla/FluidRenderMixin.java
@@ -1,7 +1,11 @@
 package mypals.ml.mixin.vanilla;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import mypals.ml.config.LucidityConfig;
 import mypals.ml.features.selectiveRendering.SelectiveRenderingManager;
 import net.minecraft.block.BlockState;
+import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.block.FluidRenderer;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.util.math.BlockPos;
@@ -33,6 +37,25 @@ public class FluidRenderMixin {
         if(!blockRenderMode.equals(SelectiveRenderingManager.RenderMode.OFF)) {
             if(!shouldRenderBlock(world.getBlockState(pos.offset(direction)),pos.offset(direction)))
                 cir.setReturnValue(false);
+        }
+    }
+    @WrapOperation(
+            method = "render",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/render/block/FluidRenderer;vertex(Lnet/minecraft/client/render/VertexConsumer;FFFFFFFFI)V"
+            )
+    )
+    private void wrapVertex(FluidRenderer instance, VertexConsumer vertexConsumer, float x, float y, float z, float red, float green, float blue, float u, float v, int light, Operation<Void> original) {
+        float alpha = LucidityConfig.fluidTransparency;
+        if(alpha == 1.01f){
+            original.call(instance, vertexConsumer, x, y, z, red, green, blue, u, v, light);
+        }else{
+            vertexConsumer.vertex(x, y, z)
+                    .color(red, green, blue, alpha)
+                    .texture(u, v)
+                    .light(light)
+                    .normal(0.0F, 1.0F, 0.0F);
         }
     }
 }
