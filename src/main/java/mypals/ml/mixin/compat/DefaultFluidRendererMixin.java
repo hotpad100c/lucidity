e7075c0218ed276d5188a965fbef23fd8b2d6a8b
hotpad100c <46704388+hotpad100c@users.noreply.github.com>
2025-07-20 20:47:01 +0900

    1.21.4feature-complete

--- src/main/java/mypals/ml/mixin/compat/DefaultFluidRendererMixin.java
+++ src/main/java/mypals/ml/mixin/compat/DefaultFluidRendererMixin.java
@@ -1,13 +1,21 @@
 package mypals.ml.mixin.compat;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import mypals.ml.config.LucidityConfig;
 import mypals.ml.features.selectiveRendering.SelectiveRenderingManager;
 import net.caffeinemc.mods.sodium.client.render.chunk.compile.pipeline.DefaultFluidRenderer;
+import net.caffeinemc.mods.sodium.client.render.chunk.terrain.material.Material;
+import net.caffeinemc.mods.sodium.client.render.chunk.vertex.builder.ChunkMeshBufferBuilder;
+import net.caffeinemc.mods.sodium.client.render.chunk.vertex.format.ChunkVertexEncoder;
 import net.minecraft.block.BlockState;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.BlockRenderView;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -17,6 +25,8 @@ import static mypals.ml.features.selectiveRendering.SelectiveRenderingManager.sh
 
 @Mixin(DefaultFluidRenderer.class)
 public class DefaultFluidRendererMixin {
+    @Shadow @Final private int[] quadColors;
+
     @Inject(at = @At("RETURN"),method = "isFullBlockFluidOccluded(Lnet/minecraft/world/BlockRenderView;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/math/Direction;Lnet/minecraft/block/BlockState;Lnet/minecraft/fluid/FluidState;)Z"
             ,cancellable = true)
     private void isFullBlockFluidOccluded(BlockRenderView world, BlockPos pos, Direction dir, BlockState blockState, FluidState fluid, CallbackInfoReturnable<Boolean> cir) {
@@ -32,6 +42,32 @@ public class DefaultFluidRendererMixin {
             BlockPos pos = new BlockPos(x, y, z);
             if(!shouldRenderBlock(world.getBlockState(pos.offset(dir)),pos.offset(dir)))
                 cir.setReturnValue(true);
+        }
+    }
+    @WrapOperation(
+            method = "writeQuad",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/caffeinemc/mods/sodium/client/render/chunk/vertex/builder/ChunkMeshBufferBuilder;push([Lnet/caffeinemc/mods/sodium/client/render/chunk/vertex/format/ChunkVertexEncoder$Vertex;Lnet/caffeinemc/mods/sodium/client/render/chunk/terrain/material/Material;)V"
+            )
+    )
+    private void wrapVertex(ChunkMeshBufferBuilder instance, ChunkVertexEncoder.Vertex[] vertices, Material material, Operation<Void> original) {
+        float alpha = LucidityConfig.fluidTransparency;
+        if(alpha == 1.01f){
+            original.call(instance,vertices, material);
+        }else{
+            if (material.isTranslucent()) {
+                for (int i = 0; i < 4; i++) {
+                    int color = this.quadColors[i];
+                    int r = color & 0xFF;
+                    int g = (color >> 8) & 0xFF;
+                    int b = (color >> 16) & 0xFF;
+                    int a = (int) (alpha * 255.0F) & 0xFF;
+                    this.quadColors[i] = (a << 24) | (b << 16) | (g << 8) | r;
+                    vertices[i].color = this.quadColors[i];
+                }
+            }
+            original.call(instance,vertices, material);
         }
     }
 }
